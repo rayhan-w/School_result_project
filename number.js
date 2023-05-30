f3839524356904907f2385c0a20fd7736a24751d
Rayhan Haidar <128999717+rayhan-w@users.noreply.github.com>
2023-05-30 18:40:38 +0600

    second project

--- number.js
+++ number.js
@@ -7,13 +7,41 @@
     let c = parseInt(document.getElementById('sub3').value);
     let d = parseInt(document.getElementById('sub4').value);
 
-    if (a >= 40 && b >= 40 && c >= 40 && d >= 40) {
-        document.getElementById("remarks").innerHTML = "Pass";
+    if(a>100 || b>100 || c>100 || d>100){
+  //   document.getElementById("remarks").innerHTML = "Nan";
+  //   document.getElementById("count").innerHTML = "Nan";
+  // document.getElementById("per").innerHTML = "Nan";
+  // document.getElementById("grade").innerHTML = "Nan";
+  alert("value cannot grater than 100");
+
     }
+   
+
+  else if (a >= 40 && b >= 40 && c >= 40 && d >= 40) {
+    document.getElementById("remarks").innerHTML = "Pass";
+    
+}
+
     else {
         document.getElementById("remarks").innerHTML = "Fail";
 
   }
+
+
+ 
+
+
+//   try{
+//     if (password == "") throw "empty";
+//         if (password.length < 8) throw "Password should have at least eight characters.";
+//         if (!password.includes("@")) throw "Password should contain the '@' symbol.";
+
+// }
+// catch (err) {
+//         message.innerHTML = "Input is " + err;
+//       }
+//     }
+
 
   let count = a + b + c + d;
   let percentage = (count / 500) * 100;
@@ -21,26 +49,7 @@
   document.getElementById("count").innerHTML = count;
   document.getElementById("per").innerHTML = percentage;
 
-
     
-//   if (percentage >= 80) {
-//     document.getElementById("grade").innerHTML = "A+";
-//   }
-//    else if (percentage >= 70) {
-//     document.getElementById("grade").innerHTML = "A";
-//   } 
-//   else if (percentage >= 60) {
-//     document.getElementById("grade").innerHTML = "A-";
-//   }
-//    else if (percentage >= 50) {
-//     document.getElementById("grade").innerHTML = "B";
-//   }
-//    else if (percentage >= 40) {
-//     document.getElementById("grade").innerHTML = "C";
-//   }
-//    else {
-//     document.getElementById("grade").innerHTML = "F";
-//   }
     if(a>=80 && b>=80 && c>=80 && d>=80){
         document.getElementById("grade").innerHTML = "A+";
     }
@@ -64,5 +73,8 @@
     else {
             document.getElementById("grade").innerHTML = "F";
           }
+
+
+
 
   }
